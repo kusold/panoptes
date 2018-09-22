b9f2a910d0c22810c53a5aae2fbd4d095c52eb30
Mike Kusold <git@mikekusold.com>
2018-09-22 11:01:33 -0600

    allow to overwrite config

--- packages/panoptes/next.config.js
+++ packages/panoptes/next.config.js
@@ -1,12 +1,21 @@
-require('dotenv').config();
 const withImages = require('next-images')
+const fs = require('fs');
+
+const dotenv = require('dotenv');
+dotenv.config();
+const privateEnv = dotenv.parse(fs.readFileSync('.env.private'))
+for (var k in privateEnv) {
+  process.env[k] = privateEnv[k]
+}
+
+
 
 const config = {
 	serverRuntimeConfig: { // Will only be available on the server side
     mySecret: 'secret'
   },
   publicRuntimeConfig: { // Will be available on both server and client
-		host: `${process.env.HOST}`,
+    host: `${process.env.HOST}`,
   },
 };
 
